70b85beb52df8a3feaeb1b106db388b1fe31d304
Nour_Khalaf <nour.n.khalaf@gmail.com>
2021-11-28 14:07:57 +0200

    Bmi app

---
+++ app/src/main/java/com/bmi/bmi/AddNewRecordActivity.java
@@ -0,0 +1,14 @@
+package com.bmi.bmi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddNewRecordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_record);
+    }
+}
