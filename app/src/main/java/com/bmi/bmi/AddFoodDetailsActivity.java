5c18073562dca6293144670fb1d4f7842d22bde0
Nour_Khalaf <nour.n.khalaf@gmail.com>
2021-12-12 20:41:03 +0200

    Bmi app

---
+++ app/src/main/java/com/bmi/bmi/AddFoodDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.bmi.bmi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddFoodDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_food_details);
+    }
+}
